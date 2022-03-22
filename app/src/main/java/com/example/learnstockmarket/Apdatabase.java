fd3999a05b43abc2d6b386d79a8258f1e576145e
shahriar021 <shahriarc19@gmail.com>
2022-03-23 02:58:42 +0600

    database added

--- app/src/main/java/com/example/learnstockmarket/Apdatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.learnstockmarket;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {User.class}, version = 2)
-public abstract class Apdatabase extends RoomDatabase {
-    public abstract UDao userDao();
-}
--- app/src/main/java/com/example/learnstockmarket/records.java
+++ app/src/main/java/com/example/learnstockmarket/records.java
@@ -1,9 +1,14 @@
 package com.example.learnstockmarket;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,52 +17,61 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.learnstockmarket.db.AppDatabase;
+import com.example.learnstockmarket.db.User;
+
+import java.util.List;
+
 public class records extends AppCompatActivity {
+    Button addNewuserbtn;
 
-    EditText e1,e2,e3,e4,e5,e6,e7;
-    TextView t1;
-    Button b1;
+    private UserListAdapter userListAdapter;
 
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_records);
-
-        e1=findViewById(R.id.idbo);
-        e2=findViewById(R.id.idstockname);
-        e3=findViewById(R.id.idquntity);
-        e4=findViewById(R.id.idbyngprc);
-        e5=findViewById(R.id.idsellingprice);
-        //e6=findViewById(R.id.spinner);
-        e7=findViewById(R.id.profitloss);
-
-        t1=findViewById(R.id.tx1);
-
-        b1=findViewById(R.id.savebtn);
-
-        b1.setOnClickListener(new View.OnClickListener() {
+        addNewuserbtn=findViewById(R.id.addbtn);
+        addNewuserbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivityForResult(new Intent(records.this,AddnewUser.class),100);
 
-                Apdatabase db = Room.databaseBuilder(getApplicationContext(),
-                        Apdatabase.class, "roomdb").build();
-                UDao userDao =db.userDao();
-                userDao.insertrecrd(new User(Integer.parseInt(e1.getText().toString()),e2.getText().toString(),e3.getText().toString(),e4.getText().toString(),e5.getText().toString(),e7.getText().toString()));
-                e1.setText("");
-                e2.setText("");
-                t1.setText("inserted successfully");
             }
         });
 
+        initRecycleView();
+        loadUserList();
 
 
 
-        /*Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.types, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);*/
+
+
+    }
+
+    private void initRecycleView(){
+        RecyclerView recyclerView = findViewById(R.id.recylerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        userListAdapter = new UserListAdapter(this);
+        recyclerView.setAdapter(userListAdapter);
+    }
+
+    private void loadUserList(){
+        AppDatabase db=AppDatabase.getDbInstance(this.getApplicationContext());
+        List<User> userList= db.userDao().getAllUsers();
+        userListAdapter.setUserList(userList);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 100){
+            loadUserList();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 
