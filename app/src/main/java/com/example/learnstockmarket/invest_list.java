7ce992de0cb4a37192dabd7af0ae5c9b29c528f7
shahriar021 <shahriarc19@gmail.com>
2022-04-18 01:23:02 +0600

    investment

--- app/src/main/java/com/example/learnstockmarket/invest_list.java
+++ app/src/main/java/com/example/learnstockmarket/invest_list.java
@@ -26,23 +26,23 @@ public class invest_list extends AppCompatActivity {
 
     private void initData() {
         userList=new ArrayList<>();
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("how to choose best stock?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
-        userList.add(new model("what is stock market ?","know here ,u wanna be rich ,invest right now,or do some trading"));
+        userList.add(new model("Rule 1","Bulls, Bears Make Money, Pigs Get Slaughtered . "));
+        userList.add(new model("Rule 2","It's OK to Pay the Taxes"));
+        userList.add(new model("Rule 3","Don't Buy All at Once"));
+        userList.add(new model("Rule 4","Buy Damaged Stocks, Not Damaged Companies"));
+        userList.add(new model("Rule 5","Diversify to Control Risk"));
+        userList.add(new model("Rule 6","Do Your Stock Homework"));
+        userList.add(new model("Rule 7","No One Made a Dime by Panicking"));
+        userList.add(new model("Rule 8","Buy Best-of-Breed Companies"));
+        userList.add(new model("Rule 9","Defend Some Stocks, Not All"));
+        userList.add(new model("Rule 10","Bad Buys Won't Become Takeovers"));
+        userList.add(new model("Rule 11","Don't Own Too Many Names"));
+        userList.add(new model("Rule 12","Cash Is for Winners"));
+        userList.add(new model("Rule 13","Expect, Don't Fear Corrections"));
+        userList.add(new model("Rule 14","Never Subsidize Losers With Winners"));
+        userList.add(new model("Rule 15","Beware of Wall Street Hype"));
+        userList.add(new model("Rule 16","There's Always a Bull Market"));
+        userList.add(new model("Rule 17","Be Flexible"));
     }
 
     private void inRecycleview() {
